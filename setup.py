6f8ed95bfc2e8c94e33d94d32d31ef62da726bc2
Rahul Singh <skrsinghrahul@gmail.com>
2020-11-17 23:28:16 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 description = 'Generic model field for storing big integer field.'
 
 try:
-    with open('README.rst') as f:
+    with open('README.md') as f:
         long_description = f.read()
 except IOError:
     long_description = description
